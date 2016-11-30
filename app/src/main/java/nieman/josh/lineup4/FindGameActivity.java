710a6f222beadafd52880d8fdadb2ed7eb87b969
Josh Nieman <joshnieman@Joshs-MacBook-Air.local>
2016-11-30 18:10:01 -0500

    three person game added

--- app/src/main/java/nieman/josh/lineup4/FindGameActivity.java
+++ app/src/main/java/nieman/josh/lineup4/FindGameActivity.java
@@ -3,7 +3,6 @@ package nieman.josh.lineup4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +14,7 @@ public class FindGameActivity extends AppCompatActivity implements View.OnClickL
     Button mFindGameButton;
     Button mGoToBoardButton;
     Button mUseBluetoothButton;
+    Button m3PlayerGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstance){
@@ -29,6 +29,9 @@ public class FindGameActivity extends AppCompatActivity implements View.OnClickL
 
         mUseBluetoothButton = (Button)findViewById(R.id.use_bluetooth_button);
         mUseBluetoothButton.setOnClickListener(this);
+
+        m3PlayerGameButton = (Button) findViewById(R.id.three_player_game);
+        m3PlayerGameButton.setOnClickListener(this);
     }
 
     @Override
@@ -39,11 +42,14 @@ public class FindGameActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.go_to_board_button:
                 //Log.d("TAG","clicked the go to board button");
-                startActivity(new Intent(this, BoardGameActivity.class));
+                startActivity(new Intent(this, BoardGame2PlayerActivity.class));
                 break;
             case R.id.use_bluetooth_button:
                 Intent goToBluetooth = new Intent(getApplicationContext(),BluetoothActivity.class);
                 startActivity(goToBluetooth);
+                break;
+            case R.id.three_player_game:
+                startActivity(new Intent(this, BoardGame3PlayerActivity.class));
                 break;
         }
     }
