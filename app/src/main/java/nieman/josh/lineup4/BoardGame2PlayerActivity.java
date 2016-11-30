c44a367456edb93d7755f2f81c40cf07467ead3d
Josh Nieman <joshnieman@Joshs-MacBook-Air.local>
2016-11-30 18:56:11 -0500

    new board game classes

---
+++ app/src/main/java/nieman/josh/lineup4/BoardGame2PlayerActivity.java
@@ -0,0 +1,102 @@
+package nieman.josh.lineup4;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.content.SharedPreferences;
+import android.widget.TextView;
+
+/**
+ * Created by joshnieman on 10/14/16.
+ */
+public class BoardGame2PlayerActivity extends Activity {
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    SharedPreferences sharedpreferences;
+    public static final String Name1 = "name1Key";
+    public static final String Name2 = "name2Key";
+
+    public static final String colorKey = "colorKey";
+
+    private GameBoard2Player mGameboard;
+    //private View mLinearLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String name1 = sharedpreferences.getString(Name1, "Player");
+        String name2 = sharedpreferences.getString(Name2, "Guest");
+
+        String color = sharedpreferences.getString(colorKey,"black");
+
+        int player1Color = Color.RED; // default
+        if(color.equals("black")) {
+            player1Color = Color.BLACK;
+        }else if(color.equals("blue")){
+            player1Color = Color.BLUE;
+        }else if(color.equals("green")){
+            player1Color = Color.GREEN;
+        }
+
+        //setContentView(R.layout.activity_game_board);
+        mGameboard = new GameBoard2Player(getApplicationContext());
+        mGameboard.setPlayer1Name(name1);
+        mGameboard.setPlayer1Color(player1Color);
+        mGameboard.setSizeOfBoard(7,6);
+
+        mGameboard.setPlayer2Name(name2);
+
+        LinearLayout linearLayout = new LinearLayout(getApplicationContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setGravity(Gravity.CENTER);
+
+        TextView player1Name = new TextView(getApplicationContext());
+        player1Name.setText(mGameboard.getPlayer1Name());
+        player1Name.setTextColor(mGameboard.getPlayer1Color());
+        player1Name.setTextSize(24);
+        player1Name.setPadding(10,10,100,10);
+        //player1Name.setWidth(700);
+
+        TextView player2Name = new TextView(getApplicationContext());
+        player2Name.setText(mGameboard.getPlayer2Name());
+        player2Name.setTextColor(mGameboard.getPlayer2Color());
+        player2Name.setTextSize(24);
+        player2Name.setPadding(100,10,10,10);
+
+
+        LinearLayout namesLayout = new LinearLayout(getApplicationContext());
+
+        namesLayout.setOrientation(LinearLayout.HORIZONTAL);
+        namesLayout.addView(player1Name);
+        namesLayout.addView(player2Name);
+
+        //linearLayout.addView(player1Name, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(namesLayout, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(getApplicationContext(),Gravity.CENTER_VERTICAL);
+        linearLayout.addView(mGameboard,1000,1000);//, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        setContentView(linearLayout);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+
+    }
+}
