80f6ba9625c7cd9440c78f493c23f94cd3da103a
Josh Nieman <joshnieman@Joshs-MacBook-Air.local>
2016-12-12 17:19:11 -0500

    added shake to welcome screen

--- app/src/main/java/nieman/josh/lineup4/WelcomeActivity.java
+++ app/src/main/java/nieman/josh/lineup4/WelcomeActivity.java
@@ -3,8 +3,13 @@ package nieman.josh.lineup4;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +51,11 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     ImageView iv_profile_pic;
     public static final String NAMES = "Names";
 
+    //sensor stuff
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,13 +68,13 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         SettingsButton.setOnClickListener(this);
         info = (TextView) findViewById(R.id.info);
         tv_profile_name = (TextView) findViewById(R.id.tv_profile_name);
-       // iv_profile_pic = (ImageView) findViewById(R.id.iv_profile_pic);
+        // iv_profile_pic = (ImageView) findViewById(R.id.iv_profile_pic);
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-              //  info.setText("Login attempt successful.");
+                //  info.setText("Login attempt successful.");
                    /*info.setText("User ID:  " +
                         loginResult.getAccessToken().getUserId() + "\n" +
                         "Auth Token: " + loginResult.getAccessToken().getToken());*/
@@ -83,14 +93,14 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                                     // All objects are from android.context.Context
                                     SharedPreferences settings = getSharedPreferences("NAMES", Context.MODE_PRIVATE);
                                     SharedPreferences.Editor editor = settings.edit();
-                                    editor.putString("name",name);
+                                    editor.putString("name", name);
                                     // Commit the edits!
                                     editor.commit();
                                     tv_profile_name.setText(name);
 
                                     String imageurl = "https://graph.facebook.com/" + id + "/picture?type=large";
 
-                                  //  Picasso.with(WelcomeActivity.this).load(imageurl).into(iv_profile_pic);
+                                    //  Picasso.with(WelcomeActivity.this).load(imageurl).into(iv_profile_pic);
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -105,7 +115,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 request.executeAsync();
 
 
-
 /**
  * AccessTokenTracker to manage logout
  */
@@ -115,11 +124,12 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                                                                AccessToken currentAccessToken) {
                         if (currentAccessToken == null) {
                             tv_profile_name.setText("");
-                           // iv_profile_pic.setImageResource(R.drawable.maleicon);
+                            // iv_profile_pic.setImageResource(R.drawable.maleicon);
                         }
                     }
                 };
             }
+
 
             @Override
             public void onCancel() {
@@ -131,6 +141,44 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 info.setText("Login attempt failed.");
             }
         });
+
+        //sensor stuff
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.
+				 */
+
+                startNewGame();
+            }
+        });
+    }
+
+
+    public void startNewGame(){
+        startActivity(new Intent(this, BoardGame2PlayerActivity.class));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 
     @Override
@@ -151,5 +199,73 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+
+    //sensor stuff
+    public interface OnShakeListener {
+        public void onShake(int count);
+    }
+
+    public class ShakeDetector implements SensorEventListener {
+
+        /*
+         * The gForce that is necessary to register as shake.
+         * Must be greater than 1G (one earth gravity unit).
+         * You can install "G-Force", by Blake La Pierre
+         * from the Google Play Store and run it to see how
+         *  many G's it takes to register a shake
+         */
+        private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+        private static final int SHAKE_SLOP_TIME_MS = 500;
+        private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+        private OnShakeListener mListener;
+        private long mShakeTimestamp;
+        private int mShakeCount;
+
+        public void setOnShakeListener(OnShakeListener listener) {
+            this.mListener = listener;
+        }
+
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // ignore
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            if (mListener != null) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                float gX = x / SensorManager.GRAVITY_EARTH;
+                float gY = y / SensorManager.GRAVITY_EARTH;
+                float gZ = z / SensorManager.GRAVITY_EARTH;
+
+                // gForce will be close to 1 when there is no movement.
+                float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+                if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                    final long now = System.currentTimeMillis();
+                    // ignore shake events too close to each other (500ms)
+                    if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                        return;
+                    }
+
+                    // reset the shake count after 3 seconds of no shakes
+                    if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                        mShakeCount = 0;
+                    }
+
+                    mShakeTimestamp = now;
+                    mShakeCount++;
+
+                    mListener.onShake(mShakeCount);
+                }
+            }
+        }
+    }
 
 }
